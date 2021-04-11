9feb7447a7a9a5f127d6a9ee8edce91bdc1fff32
benstr1 <benstr1@ktu.lt>
2021-04-11 09:15:56 +0300

    Db sutvarkymas

--- DarbasGamykloje/Global.asax.cs
+++ DarbasGamykloje/Global.asax.cs
@@ -17,5 +17,10 @@ namespace DarbasGamykloje
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+    }
+    public static class Globals
+    {
+        public static string dbPrefix { get { return "pavyzdys_"; } }
     }
 }
