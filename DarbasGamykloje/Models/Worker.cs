86467eb25f6e5782ec1e0b28783815d958df23ac
edvrie <61147298+edvrie@users.noreply.github.com>
2021-05-14 20:42:11 +0300

    M

--- DarbasGamykloje/Models/Worker.cs
+++ DarbasGamykloje/Models/Worker.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DarbasGamykloje.Models
 {
     public class Worker
     {
+        public int salary { get; set; }
+        public bool isDeleted { get; set; }
+        public int numberOfDaysWorked { get; set; }
+        public int checkedSalaryCount { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id_Worker { get; set; }
+        public int fk_Managerid_Manager { get; set; }
+        public int fk_LivingSpaceid_LivingSpace { get; set; }
+        public int fk_RegisteredUserid_RegisteredUser { get; set; }
+        public int fk_Factoryid_Factory { get; set; }
     }
 }
---
+++ DarbasGamykloje/ViewModels/Worker/WorkerView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace DarbasGamykloje.ViewModels
+{
+    public class WorkerView
+    {
+        [DisplayName("Salary")]
+        public int salary { get; set; }
+        [DisplayName("Is deleted")]
+        public bool isDeleted { get; set; }
+        [DisplayName("Number of days worked")]
+        public int numberOfDaysWorked { get; set; }
+        [DisplayName("Checked salary count")]
+        public int checkedSalaryCount { get; set; }
+        [DisplayName("Worker ID")]
+        public int id_Worker { get; set; }
+        [DisplayName("Manager ID")]
+        public int fk_Managerid_Manager { get; set; }
+        [DisplayName("Living space ID")]
+        public int fk_LivingSpaceid_LivingSpace { get; set; }
+        [DisplayName("User ID")]
+        public int fk_RegisteredUserid_RegisteredUser { get; set; }
+        [DisplayName("Factory ID")]
+        public int fk_Factoryid_Factory { get; set; }
+    }
+}
